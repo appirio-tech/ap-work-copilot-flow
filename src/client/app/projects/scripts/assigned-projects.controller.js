41827aa8cfa520f2e2768caf48a7b7cc1399f7e6
samsep <ssepasi@appirio.com>
2015-06-09 09:31:11 -0700

    card layout for projects

--- src/client/app/projects/scripts/assigned-projects.controller.js
+++ src/client/app/projects/scripts/assigned-projects.controller.js
@@ -3,51 +3,32 @@
 
   angular
     .module('app.projects')
-    .controller('AssignedProjectsController', AssignedProjectsController);
+    .controller('ProjectsController', ProjectsController);
 
-  AssignedProjectsController.$inject = ['copilotAssignedProjects', '$state'];
+  ProjectsController.$inject = ['workRequests', '$state'];
   /* @ngInject */
-  function AssignedProjectsController(copilotAssignedProjects, $state) {
+  function ProjectsController(workRequests, $state) {
     var vm = this;
-    vm.title = 'Work Requests';
+    vm.title = 'View Projects';
     vm.workRequests = [];
-    vm.newProject = null;
     vm.formatWorkRequests = null;
-    vm.go = null;
-    vm.showMessage = false;
 
     vm.activate = function() {
-      vm.copilotAssignedProjects = vm.formatProjects(copilotAssignedProjects);
-      vm.showMessage = false;
+      vm.workRequests = vm.formatWorkRequests(workRequests);
     };
 
-   vm.formatProjects= function(requests) {
-     var statusClasses = {
-       'Incomplete': 'incomplete',
-       'Submitted' : 'submitted'
-     };
-     var statusMessages = {
-       'Incomplete': 'PROJECT SUBMISSION INCOMPLETE',
-       'Submitted' : 'PROJECT SUBMITTED'
-     };
-     var checkmarks = {
-       'Submitted': 'check-solid-blue.svg',
-     };
-     var typeDisplays = {
-       'design': 'Mobile: Design',
-       'code'  : 'Mobile: Code',
-       'design & code': 'Design & Code'
-     };
+    vm.formatWorkRequests = function(requests) {
+      var typeDisplays = {
+        'design': 'Mobile: Design',
+        'code'  : 'Mobile: Code',
+        'design & code': 'Design & Code'
+      };
 
-     return requests.map(function(work) {
-       work.status      = work.status || 'Incomplete';
-       work.class       = statusClasses[work.status];
-       work.message     = statusMessages[work.status];
-       work.checkmark   = checkmarks[work.status];
-       work.requestType = typeDisplays[work.requestType];
-       return work;
-     });
-   };
+      return requests.map(function(work) {
+        work.requestType = typeDisplays[work.requestType];
+        return work;
+      });
+    };
 
     vm.activate();
 
