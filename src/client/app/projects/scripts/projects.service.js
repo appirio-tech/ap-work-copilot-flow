fb05344f30f2e336afd361991461df2cdbed46b7
samsep <ssepasi@appirio.com>
2015-06-26 10:42:13 -0700

    fix assigned mapping bug

--- src/client/app/projects/scripts/projects.service.js
+++ src/client/app/projects/scripts/projects.service.js
@@ -12,6 +12,7 @@
     var service = {
 
       openPromises: [],
+      promises: [],
 
       // functions
       getWorkRequests        : null,
@@ -32,29 +33,32 @@
 
     service.getAssignedProjects = function() {
       var deferred = $q.defer();
-      data.get('copilot-assigned-projects', {copilotId: UserService.currentUser.id}).then(function(data) {
-        deferred.resolve(data.result.content);
-       //  var promises = data.result.content.map(function(project) {
+      data.get('copilot-assigned-projects', {copilotId: UserService.currentUser.id}).then(function(copilotData) {
+        service.openPromises = copilotData.result.content;
+        var promises = copilotData.result.content.map(function(project) {
        //   return $http({
        //     method: 'GET',
        //     url: 'https://api.topcoder-dev.com/v3/app-work-requests/',
        //     params: { filter: 'copilotId=unassigned&id='+project.id}
        //      })
        // });
-       //  $q.all(promises)
-       //  .then(function(data) {
-       //    console.log(data)
-       //    data.forEach(function(dataProject) {
-       //      openPromises.forEach(function(project) {
-       //        if (project.id === dataProject.data.result.content[0].id) {
-       //          project.name = dataProject.data.result.content[0].name;
-       //          project.createdAt = dataProject.data.result.content[0].createdAt;
-       //          project.requestType = dataProject.data.result.content[0].requestType;
-       //        }
-       //      })
-       //    })
-       //    deferred.resolve(openPromises)
-       //   })
+        return data.get('copilot-work-request', {id: project.id}).then(function(workData) {
+          return workData.result.content;
+        });
+        })
+        $q.all(promises)
+        .then(function(data) {
+          data.forEach(function(dataProject) {
+            service.openPromises.forEach(function(project) {
+              if (project.id === dataProject.id) {
+                project.name = dataProject.name;
+                project.createdAt = dataProject.createdAt;
+                project.requestType = dataProject.requestType;
+              }
+            })
+          })
+          deferred.resolve(service.openPromises)
+         })
         })
         return deferred.promise;
        }
