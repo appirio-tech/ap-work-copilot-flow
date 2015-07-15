72b24ca8f1ed5f2b90d7da3946ce70022193ae27
samsep <ssepasi@appirio.com>
2015-07-15 10:23:40 -0700

    project-details service specs

--- src/client/app/project-details/scripts/project-details.service.spec.js
+++ src/client/app/project-details/scripts/project-details.service.spec.js
@@ -1,10 +1,11 @@
 /* jshint -W117, -W030 */
-// describe.only('ProjectsDetailsService', function () {
+// describe.only('ProjectDetailsService', function () {
 //   var service
 
 //   beforeEach(function () {
-//     bard.inject(this, 'ProjectsDetailsService');
-//     service = ProjectsDetailsService;
+//     // bard.appModule('app.project-details')
+//     bard.inject(this, '$q', 'ProjectDetailsService')
+//     service = ProjectDetailsService;
 //   });
 
 //   bard.verifyNoOutstandingHttpRequests();
@@ -13,5 +14,21 @@
 //     it('should be created successfully', function () {
 //       expect(service).to.be.defined;
 //     });
+
+//     describe('initializeCopilotWork method', function() {
+//       it ('should update project status', function() {
+//         service.initializeCopilotWork('123', 'claimed');
+//         expect(service.workDetails['123']['status']).to.equal('claimed')
+//     })
+//     })
 //   });
 // });
+
+
+
+
+// bard.mockService(ProjectDetailsService, {
+//       initalizeCopilotWork: $q.when([{id: '123'}]),
+//       work: {id: '123'},
+//       _default: $q.when({})
+//     });
