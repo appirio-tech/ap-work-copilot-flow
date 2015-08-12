5fbeda37eaa71fa86e555ed17de4586b3114522c
samsep <ssepasi@appirio.com>
2015-08-12 15:42:38 -0700

    modify project service specs

--- app/tests/projects.service.spec.js
+++ app/tests/projects.service.spec.js
@@ -3,11 +3,15 @@
    var service, requests;
 
    beforeEach(function () {
-     bard.inject(this, 'ProjectsService');
+     bard.inject(this, 'ProjectsService', '$q', '$http');
    });
 
    beforeEach(function () {
      service = ProjectsService;
+
+      bard.mockService($http, {
+      _default: $q.when({})
+    })
    });
 
    bard.verifyNoOutstandingHttpRequests();
@@ -16,6 +20,19 @@
      it('should be created successfully', function () {
        expect(service).to.be.defined;
      });
+
+     it('should have a getWorkRequests method', function () {
+       expect(service.getWorkRequests).to.exist;
+     });
+
+     // it('should make an http call for work requests', function() {
+     //  service.getWorkRequests()
+     //  expect($http.called).to.be.ok
+     // })
+
+    it('should have a getAssignedProjects method', function () {
+      expect(service.getAssignedProjects).to.exist;
+    });
 
    });
  });
