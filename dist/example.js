9f1a13680f47806c1ab1ecb61d8878e3893f17c5
Alex Tran <vietnogi@gmail.com>
2015-09-17 12:08:42 -0700

    build

--- dist/example.js
+++ dist/example.js
@@ -6,9 +6,9 @@ angular.module("app.constants", [])
 
 .constant("SUBMISSION_URL", "https://studio.topcoder.com")
 
-.constant("AUTH0_CLIENT_ID", "abc123")
+.constant("AUTH0_CLIENT_ID", "Auth0_Client_Id")
 
-.constant("AUTH0_DOMAIN", "topcoder.auth0.com")
+.constant("AUTH0_DOMAIN", "topcoder-domain.auth0.com")
 
 .constant("AUTH0_TOKEN_NAME", "userJWTToken")
 
